c4c9620459719d0c376819877c74b3db5c836b24
Korneliu <olbiton@gmail.com>
2017-06-16 08:18:30 -0700

    html set up

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	//data object 
+
+	//initialize variables
+
+
+	//question display function
+
+
+	//submit form listener
+
+
+	//show results function
+
+
+
+
+
+
+
+
+
+})
